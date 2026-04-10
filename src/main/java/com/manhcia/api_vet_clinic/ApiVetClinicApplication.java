68dc7971909145bbfed2da108266ec6b1e9fe286
Ian Manhica <ianmanhica4@gmail.com>
2026-04-10 20:03:28 +0200

    first commit

---
+++ src/main/java/com/manhcia/api_vet_clinic/ApiVetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.manhcia.api_vet_clinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiVetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiVetClinicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/manhcia/api_vet_clinic/ApiVetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manhcia.api_vet_clinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiVetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
